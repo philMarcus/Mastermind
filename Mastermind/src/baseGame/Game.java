962387cc8dc8462bb538df9d630386535e6b8d04
Phil Marcus <pmarcus@reverek12.org>
2022-09-10 20:39:13 -0400

    Added Reset Game menu functionality

--- Mastermind/src/baseGame/Game.java
+++ Mastermind/src/baseGame/Game.java
@@ -5,6 +5,8 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -14,48 +16,48 @@ import javax.swing.JRadioButtonMenuItem;
 
 import ai.AI;
 
-public class Game implements ActionListener{
-	
+public class Game implements ActionListener {
+
 	private ArrayList<Turn> turns = new ArrayList<Turn>();
 	private GameSettings settings = new GameSettings();
-	//initializes a random secret code
-	private Code secretCode = new Code(settings.getCodeLength(),settings.getPegOptions());
-	
+	// initializes a random secret code
+	private Code secretCode = new Code(settings.getCodeLength(), settings.getPegOptions());
+
 	private static AI ai;
 
-	
-	//GUI components
+	// GUI components
 	private static JFrame window;
 	private static GuessInputPanel guessPanel;
 	private static Board board;
 	private static GameMenuBar menuBar;
 
-	
 	public void takeTurn(Code guess) {
-		//add a new turn to the gamestate, which consists of a guessed code and a calculated response
+		// add a new turn to the gamestate, which consists of a guessed code and a
+		// calculated response
 		turns.add(new Turn(guess, new Response(guess, secretCode)));
 		// update the board display with the new turn
-		board.addTurn(turns.get(turns.size()-1));
-		//check the new turn for victory
-		if (turns.get(turns.size()-1).isVictory()){
-			JOptionPane.showMessageDialog(window, "Win! The secret code was indeed"+secretCode);
+		board.addTurn(turns.get(turns.size() - 1));
+		// check the new turn for victory
+		if (turns.get(turns.size() - 1).isVictory()) {
+			JOptionPane.showMessageDialog(window, "Win! The secret code was indeed" + secretCode);
 			reset();
 		}
-		//check new turn for a loss
-		else if (turns.size() >= settings.getMaxTries() ) {
-			JOptionPane.showMessageDialog(window, "Lose. The secret code was"+secretCode);
+		// check new turn for a loss
+		else if (turns.size() >= settings.getMaxTries()) {
+			JOptionPane.showMessageDialog(window, "Lose. The secret code was" + secretCode);
 			reset();
 
 		}
 
 	}
-	
-	// clear the board and gamestate, select new secret code, and reset the AI analysis
+
+	// clear the board and gamestate, select new secret code, and reset the AI
+	// analysis
 	public void reset() {
 		board.clear();
 		turns.clear();
 		ai = new AI(getSettings());
-		secretCode = new Code(settings.getCodeLength(),settings.getPegOptions());
+		secretCode = new Code(settings.getCodeLength(), settings.getPegOptions());
 	}
 
 	public ArrayList<Turn> getTurns() {
@@ -70,7 +72,6 @@ public class Game implements ActionListener{
 		return secretCode;
 	}
 
-
 	private static void createAndShowGUI() {
 		Game game = new Game();
 		ai = new AI(game.getSettings());
@@ -78,56 +79,57 @@ public class Game implements ActionListener{
 
 		// Set this window's location and size:
 		window.setBounds(300, 300, 500, 600);
-		
+
 		menuBar = new GameMenuBar();
 		window.setJMenuBar(menuBar);
+		menuBar.reset.addActionListener(game);
 
-		//  Create a Board, which is a kind of JPanel:
+		// Create a Board, which is a kind of JPanel:
 		board = new Board(game);
 
 		// Create the user input panel
 		guessPanel = new GuessInputPanel(game);
 
-		
 		// Add panels to window:
 		Container c = window.getContentPane();
 		c.setLayout(new FlowLayout());
 		c.add(board);
 		c.add(guessPanel);
-		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);		
-	}
-	
 
-	public void actionPerformed(ActionEvent e) { 
-	    //Registered on the "take turn" button on user input panel. Takes a turn.
-		Code guess = guessPanel.getUserCode();
-		takeTurn(guess);
-		
-		//test new AI code
-		ai.analyze(turns);
-		System.out.println(ai.toString());
-		
-	}	
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// Registered on the "take turn" button on user input panel. Takes a turn.
+		if (e.getSource().equals(menuBar.reset)) {
+			reset();
+		} else {
+			Code guess = guessPanel.getUserCode();
+			takeTurn(guess);
+
+			// test new AI code
+			ai.analyze(turns);
+			System.out.println(ai.toString());
+		}
+	}
 
 	public static void main(String[] args) {
 
-	      javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	                createAndShowGUI();
-	            }
-	        });
-	}	
-	
-	
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
 }
 
 class GameMenuBar extends JMenuBar {
 	JMenu game, difficulty;
 	JMenuItem reset;
 	JRadioButtonMenuItem easy, normal, hard;
-	
+
 	public GameMenuBar() {
 		game = new JMenu("Game");
 		difficulty = new JMenu("Chage Difficulty");
@@ -140,7 +142,7 @@ class GameMenuBar extends JMenuBar {
 		difficulty.add(easy);
 		difficulty.add(normal);
 		difficulty.add(hard);
-		game.add(difficulty);			
-		
-		}
+		game.add(difficulty);
+
+	}
 }
