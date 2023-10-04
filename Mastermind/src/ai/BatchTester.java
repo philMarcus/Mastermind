7739a6703f02bc08bed98131dd7e23b1387063ad
Phil Marcus <pmarcus@reverek12.org>
2023-10-04 14:56:16 -0400

    tested TheProfessor AI 100,000 times

--- Mastermind/src/ai/BatchTester.java
+++ Mastermind/src/ai/BatchTester.java
@@ -5,7 +5,7 @@ import baseGame.GameSettings;
 public class BatchTester {
 
 	public static void main(String[] args) {
-		final int GAMES = 1000;
+		final int GAMES = 100000;
 		GameSettings settings = new GameSettings();
 		AIBatchGames batch = new AIBatchGames(settings);
 		for (int i = 0; i < GAMES; i++) {
--- Mastermind/src/ai/TheProfessorAI.java
+++ Mastermind/src/ai/TheProfessorAI.java
@@ -3,8 +3,20 @@ package ai;
 import baseGame.Code;
 
 //Testing results:
-//Won 10000 out of 10000 games. 
-//Mean attempts to win: 4.6437
+//Games Won In: 
+//1 turns: 72
+//2 turns: 988
+//3 turns: 7655
+//4 turns: 32874
+//5 turns: 44359
+//6 turns: 13065
+//7 turns: 974
+//8 turns: 13
+//9 turns: 0
+//10 turns: 0
+//Won 100000 out of 100000 games. 
+//Mean attempts to win: 4.63616
+//Median attempts to win: 5
 
 public class TheProfessorAI implements AIPersonality {
 private AnalyzedGame game;
