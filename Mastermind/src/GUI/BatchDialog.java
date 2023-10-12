a80e15860c5b2e445ec38779203ea6b354698f4a
Phil Marcus <pmarcus@reverek12.org>
2023-10-12 10:51:41 -0400

    Added attributes to BatchDialog and BatchInputPanel

--- Mastermind/src/GUI/BatchDialog.java
+++ Mastermind/src/GUI/BatchDialog.java
@@ -2,13 +2,28 @@ package GUI;
 
 import java.util.EventListener;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.SpinnerNumberModel;
 
 public class BatchDialog extends JDialog {
 
+	BatchInputPanel inputPanel;
+	JTextArea batchText;
 }
 
+
 class BatchInputPanel extends JPanel implements EventListener{
+	
+	SpinnerNumberModel lenModel;
+	JSpinner lenInput;
+	SpinnerNumberModel numPegsModel;
+	JSpinner numPegsInput;
+	SpinnerNumberModel numGamesModel;
+	JSpinner numGamesInput;
+	JButton run;
 	
 }
