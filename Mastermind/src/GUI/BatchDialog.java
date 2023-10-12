71d35e3b36cbd9a481e54af52298693c9d01c912
Phil Marcus <pmarcus@reverek12.org>
2023-10-12 10:25:08 -0400

    Created BatchDialog public class
    created private class BatchInputPanel also

---
+++ Mastermind/src/GUI/BatchDialog.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import java.util.EventListener;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+public class BatchDialog extends JDialog {
+
+}
+
+class BatchInputPanel extends JPanel implements EventListener{
+	
+}
--- Mastermind/src/ai/BatchTester.java
+++ Mastermind/src/ai/BatchTester.java
@@ -5,8 +5,9 @@ import baseGame.GameSettings;
 public class BatchTester {
 
 	public static void main(String[] args) {
-		final int GAMES = 100000;
+		final int GAMES = 1000;
 		GameSettings settings = new GameSettings();
+		//settings.setCodeLength(5);
 		AIBatchGames batch = new AIBatchGames(settings);
 		for (int i = 0; i < GAMES; i++) {
 			AnalyzedGame g = batch.runGame();
